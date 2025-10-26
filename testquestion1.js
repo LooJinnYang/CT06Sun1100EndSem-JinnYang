27f273a32aea3368f3aba16ebcfb9fb18f58e3a9
LooJinnYang <ljinnyang@gmail.com>
2025-10-26 03:41:07 +0000

    26/10

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,22 @@
+let x = 300;
+let y = 200;
+let size = 50;
+let speedX = 5;
 
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw(){
+    background('lightblue')
+
+x += speedX;
+
+if(x - size/2 <= 0 || x + size/2 >= width){
+    speedX *= -1;
+}
+
+noStroke();
+fill('limegreen');
+circle (x,y,size);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let array = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
 
+function setup(){
+    createCanvas(200, 300);
+    background(0);
+    }
+    
+function draw(){
+    background(0);
+
+    fill('pink');
+    textSize(20);
+    textAlign(CENTER, CENTER)
+    for(let i = 0; i < array.length; i++){
+        text(array[i], width/2, 100 + i * 30);
+    }
+}
